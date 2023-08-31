2454e7bb73841a500375cd450968943c3eb86d69
Vidhish-Trivedi <Vidhish.Trivedi@iiitb.ac.in>
2023-09-01 02:07:16 +0530

    Add Flask templating examples

--- Flask/basic-templates/main.py
+++ Flask/basic-templates/main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+###################################################################################################################
 # @app.route("/")
 # def index():
 #     name = "John"
@@ -10,13 +11,36 @@ app = Flask(__name__)
 #     nums = [2, 3, 5, 7, 11, 13]
 #     return render_template("basic.html", p_name=name, p_letters=letters, p_dictionary=dictionary, nums=nums)
 
+###################################################################################################################
+# @app.route("/")
+# def index():
+#     return render_template("home.html")
+
+# @app.route("/person/<name>")
+# def person_name(name):
+#     return render_template("person.html", name=name)
+
+###################################################################################################################
 @app.route("/")
 def index():
-    return render_template("home.html")
+    return render_template('index.html')
 
-@app.route("/person/<name>")
-def person_name(name):
-    return render_template("person.html", name=name)
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/thankyou')
+def thankyou():
+    # Name of the variable in session that stores 'First Name' is 'first'.
+    first = request.args.get('first')
+    last = request.args.get('last')
+
+    return render_template('thankyou.html', first=first, last=last)
+
+# Pass in the error code.
+@app.errorhandler(404)
+def page_not_found(err):
+    return (render_template('404.html'), 404)
 
 if __name__ == "__main__":
     app.run(debug=True)
